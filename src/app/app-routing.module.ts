74b83ca6bd10c32e4b9413be948443b657798a2d
waqas <anthraxxx00915@gmail.com>
2019-03-11 16:51:13 +0500

    dasdasd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-<<<<<<< HEAD
     // { path: '', loadChildren: './login/login.module#LoginPageModule' },
     { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -17,18 +16,3 @@ const routes: Routes = [
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
-=======
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
-  { path: 'book', loadChildren: './book/book.module#BookPageModule' }
-];
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
->>>>>>> 061291b783420add666220be7ceb0f1dc2141d3a
